252fadd76ec31e3926825d085155b82e757e2b38
TatianaKokunina <tanechka.91@mail.ru>
2022-11-28 13:59:07 +0300

    EX36

--- EX36/Program.cs
+++ EX36/Program.cs
@@ -3,3 +3,37 @@
 [3, 7, 23, 12] -> 19
 [-4, -6, 89, 6] -> 0
 */
+int sum = 0;
+int[] array = GenerateRandomArray(10, -100, 100);
+
+int[] GenerateRandomArray(int length,int startPoint, int endPoint)
+{
+    int[] resultArray = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        resultArray[i] = new Random().Next(startPoint, endPoint+1);
+    }
+    return resultArray;
+}
+
+void printArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if (i < array.Length - 1)
+        {
+            Console.Write(",");
+        }
+    }
+    Console.WriteLine("]");
+}
+
+for (int i = 0; i < array.Length; i+=2)
+    {
+       {sum += array[i];}
+    }
+
+printArray(array);
+Console.WriteLine($"В заданном массиве сумма элементов, стоящих на нечётных позициях = {sum}");
