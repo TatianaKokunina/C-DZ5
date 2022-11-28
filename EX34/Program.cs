02944cb353d55d49f31605dfb876d7aa0bf4f5d2
TatianaKokunina <tanechka.91@mail.ru>
2022-11-28 13:32:43 +0300

    EX34

--- EX34/Program.cs
+++ EX34/Program.cs
@@ -2,35 +2,38 @@
 Напишите программу, которая покажет количество чётных чисел в массиве.
 [345, 897, 568, 234] -> 2
 */
-
-Console.WriteLine("Введите размер массива");
-int size = Convert.ToInt32(Console.ReadLine());
-int[] numbers = new int[size];
-FillArrayRandomNumbers(numbers);
-Console.WriteLine("Вот наш массив: ");
-PrintArray(numbers);
 int count = 0;
+int[] array = GenerateRandomArray(10, 100, 999);
 
-for (int z = 0; z < numbers.Length; z++)
-if (numbers[z] % 2 == 0)
-count++;
-
-Console.WriteLine($"всего {numbers.Length} чисел, {count} из них чётные");
-
-void FillArrayRandomNumbers(int[] numbers)
+int[] GenerateRandomArray(int length,int startPoint, int endPoint)
 {
-    for(int i = 0; i < numbers.Length; i++)
+    int[] resultArray = new int[length];
+    for (int i = 0; i < length; i++)
     {
-        numbers[i] = new Random().Next(100,1000);
+        resultArray[i] = new Random().Next(startPoint, endPoint+1);
     }
+    return resultArray;
 }
-void PrintArray(int[] numbers)
+
+void printArray(int[] array)
 {
-    Console.Write("[ ");
-    for(int i = 0; i < numbers.Length; i++)
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
     {
-        Console.Write(numbers[i] + " ");
+        Console.Write(array[i]);
+        if (i < array.Length - 1)
+        {
+            Console.Write(",");
+        }
     }
-    Console.Write("]");
-    Console.WriteLine();
+    Console.WriteLine("]");
 }
+
+for (int i = 0; i < array.Length; i++)
+    {
+       if (array[i] % 2 == 0)
+       {count++;}
+    }
+
+printArray(array);
+Console.WriteLine($"В заданном массиве {count} четных чисел");
